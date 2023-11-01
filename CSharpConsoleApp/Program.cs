001075600eecd8ecb4e98f9c4b9bf2542f5d2b63
James Grieve <jgrieve@ualberta.ca>
2023-10-31 19:34:58 -0600

    Initial commit; Basic variable types and syntax;

---
+++ CSharpConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CSharpConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Data Types ---
+            // Integer Numeric Data Types
+            int userNumber;
+            short userShort;
+            long userLong;
+
+            // Floating Point Numeric Data
+            float userFloat;
+            double userDouble;
+
+            // Decimal Numeric Data
+            decimal userDecimal;
+
+            // Character/String Data
+            char userChar;
+            string userName;
+
+            // Boolean Data
+            bool userBool;
+
+            // -- Operators --
+            // Arithmetic Operators
+            // + - * / %
+
+            // Relational Operators
+            // > < >= <= == !=
+
+            // Logical Operators
+            // && || !
+
+
+            int leftOperand, rightOperand;
+            Console.Write("Please enter a left operand (number): ");
+            leftOperand = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a right operand (number): ");
+            rightOperand = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter a operator (+): ");
+            string operation = Console.ReadLine(); 
+
+
+            Console.WriteLine("The solution to "+leftOperand+" + "+rightOperand+" is "+(leftOperand+rightOperand));
+        }
+    }
+}
