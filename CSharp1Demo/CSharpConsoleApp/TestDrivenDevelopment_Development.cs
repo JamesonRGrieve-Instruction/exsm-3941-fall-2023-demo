ea9deab13970623da6effd1e3750b60844d5751a
James Grieve <jgrieve@ualberta.ca>
2023-11-09 19:40:01 -0700

    In-class practice;

--- CSharp1Demo/CSharpConsoleApp/TestDrivenDevelopment_Development.cs
+++ CSharp1Demo/CSharpConsoleApp/TestDrivenDevelopment_Development.cs
@@ -8,12 +8,31 @@ namespace CSharpConsoleApp
 {
     public class TestDrivenDevelopment_Development
     {
-        // Output: integer
-        // Input: integer
-        public static int MinutesToSeconds(int seconds)
+        public static int MinutesToSeconds(int minutes)
+        {
+            if (minutes < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minutes));
+            }
+            return minutes * 60;
+        }
+        public static int TimestampToSeconds(string timestamp)
         {
             throw new NotImplementedException();
         }
-
+        public static string ModifyString(string text)
+        {
+            throw new NotImplementedException();
+        }
+        public enum Shape
+        {
+            Rectangle,
+            Triangle,
+            Circle
+        }
+        public static double GetArea(Shape shape, double sideOne, double? sideTwo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CSharp1Demo/CSharpUnitTests/TestDrivenDevelopment_Tests.cs
+++ CSharp1Demo/CSharpUnitTests/TestDrivenDevelopment_Tests.cs
@@ -12,13 +12,13 @@ namespace CSharpUnitTests
         [InlineData(0, 0)]
         [InlineData(60, 1)]
         [InlineData(600, 10)]
-        public static void TestValid_MinutesToSeconds(int expectedResult, int seconds)
+        public static void TestValid_MinutesToSeconds(int expectedResult, int minutes)
         {
             // Arrange
             // N/A
 
             // Act
-            int actualResult = TestDrivenDevelopment_Development.MinutesToSeconds(seconds);
+            int actualResult = TestDrivenDevelopment_Development.MinutesToSeconds(minutes);
 
             // Assert
             Assert.Equal(expectedResult, actualResult);
@@ -43,7 +43,48 @@ namespace CSharpUnitTests
             // One is saying that the function should throw the argument exception at all.
             // The other is saying that the parameter name of that exception is correct.
             ArgumentOutOfRangeException exception = Assert.Throws<ArgumentOutOfRangeException>(act);
-            Assert.Equal("seconds", exception.ParamName);
+            Assert.Equal("minutes", exception.ParamName);
+        }
+
+        [Theory]
+        [InlineData(0, "0:00")]
+        [InlineData(60, "0:01")]
+        [InlineData(300, "0:05")]
+        [InlineData(3600, "1:00")]
+        public static void TestValid_TimeStampToSeconds(int expectedResult, string timestamp)
+        {
+            // Arrange
+            // N/A
+
+            // Act
+            int actualResult = TestDrivenDevelopment_Development.TimestampToSeconds(timestamp);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("01")]
+        [InlineData("10;10")]
+        [InlineData("One hour.")]
+        [InlineData("-1:00")]
+        [InlineData("0:-01")]
+        [InlineData("1:1:1")]
+        //[InlineData("0:1")]
+        public static void TestInvalid_TimestampToSeconds(string timestamp)
+        {
+            // Arrange
+            // N/A
+
+            // Act
+            Action act = () =>
+            {
+                TestDrivenDevelopment_Development.TimestampToSeconds(timestamp);
+            };
+
+            // Assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("timestamp", exception.ParamName);
         }
 
     }
