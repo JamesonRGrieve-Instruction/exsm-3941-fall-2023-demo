08e1e33249cbb4e267e93065becaae604998db2a
James Grieve <jgrieve@ualberta.ca>
2023-11-14 20:46:11 -0700

    Math with DateTime and TimeSpan;

--- CSharp1Demo/CSharpConsoleApp/Program.cs
+++ CSharp1Demo/CSharpConsoleApp/Program.cs
@@ -26,26 +26,30 @@ namespace CSharpConsoleApp
             int emptyInt;
 
             // Using physical and logical sizes.
-            List<int> numberArray = new List<int>();
+            List<DateTime> numberArray = new List<DateTime>();
+
+            DateTime now = DateTime.Now;
+            TimeSpan difference = new TimeSpan(8, 0, 0, 0);
+            Console.WriteLine($"Exactly one week and one day from today will be {now.Add(difference)}.");
 
             int userSelection;
             do
             {
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.BackgroundColor = ConsoleColor.White;
-                Console.Clear();
+                //Console.Clear();
                 userSelection = GetValidInt("--- Welcome to Number Storage ---\n1. Create - Add an Item\n2. Read - View the Items\n3. Update - Change an Item\n4. Delete - Remove an Item\n5. Sort - Organize the Items\n0. Exit\nPlease make a selection: ");
                 if (userSelection == 1)
                 {
                     // Create
-                    int newItemParsed = GetValidInt("Please enter a number to add: ");
-                    numberArray.Add(newItemParsed);
+                    DateTime dateTime = DateTime.Now;
+                    numberArray.Add(dateTime);
                 }
                 else if (userSelection == 2)
                 {
                     for (int i = 0; i < numberArray.Count; i++)
                     {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
+                        Console.WriteLine($"{i + 1}) {numberArray[i]}, {numberArray[i].Ticks} ticks.");
                     }
                     Console.WriteLine("Press enter to return to menu.");
                     Console.ReadLine();
@@ -58,7 +62,7 @@ namespace CSharpConsoleApp
                     }
 
                     int modifyIndexParsed = GetValidInt("Please make a selection to modify: ");
-                    int newItemParsed = GetValidInt($"Please enter a number to overwrite postion {modifyIndexParsed} ({numberArray[modifyIndexParsed - 1]}) with: ");
+                    DateTime newItemParsed = DateTime.Now;
                     numberArray[modifyIndexParsed - 1] = newItemParsed;
                 }
                 else if (userSelection == 4)
@@ -67,7 +71,7 @@ namespace CSharpConsoleApp
                     {
                         Console.WriteLine($"{i + 1}) {numberArray[i]}");
                     }
-                    int deleteIndexParsed = GetValidInt("Please make a selection to modify: ");
+                    int deleteIndexParsed = GetValidInt("Please make a selection to delete: ");
                     numberArray.RemoveAt(deleteIndexParsed - 1);
                 }
                 else if (userSelection == 5)
