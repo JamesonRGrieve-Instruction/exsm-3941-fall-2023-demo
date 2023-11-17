59e7812102b339886564215358bcb114df73a5c2
James Grieve <jgrieve@ualberta.ca>
2023-11-16 19:24:44 -0700

    Solution;

--- CSharp1Demo/CSharpConsoleApp/Program.cs
+++ CSharp1Demo/CSharpConsoleApp/Program.cs
@@ -6,92 +6,69 @@ namespace CSharpConsoleApp
     {
         static void Main(string[] args)
         {
-            // Max length of 2.
-            string[] stringArray =
-            {
-                "Hello",
-                "World"
-            };
-
-            // Max length of 10 (default values - null).
-            string[] emptyArray = new string[10];
-
-            // Null.
-            string emptyString;
-
-            // Max length of 10 (default values - 0).
-            int[] emptyIntArray = new int[10];
-
-            // 0.
-            int emptyInt;
-
-            // Using physical and logical sizes.
-            List<DateTime> numberArray = new List<DateTime>();
-
-            DateTime now = DateTime.Now;
-            TimeSpan difference = new TimeSpan(8, 0, 0, 0);
-            Console.WriteLine($"Exactly one week and one day from today will be {now.Add(difference)}.");
-
-            int userSelection;
+            int playAgain = 0;
+            Random rng = new Random();
+            Console.WriteLine("--- Higher / Lower Guessing Game ---\nEnter a maximum value to play to.");
+            int maximum = GetRangedInput(1, 1000);
             do
             {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.BackgroundColor = ConsoleColor.White;
-                //Console.Clear();
-                userSelection = GetValidInt("--- Welcome to Number Storage ---\n1. Create - Add an Item\n2. Read - View the Items\n3. Update - Change an Item\n4. Delete - Remove an Item\n5. Sort - Organize the Items\n0. Exit\nPlease make a selection: ");
-                if (userSelection == 1)
+                if (playAgain == 2)
                 {
-                    // Create
-                    DateTime dateTime = DateTime.Now;
-                    numberArray.Add(dateTime);
+                    Console.WriteLine("Enter a maximum value to play to.");
+                    maximum = GetRangedInput(1, 1000);
                 }
-                else if (userSelection == 2)
+                int target = rng.Next(1, maximum + 1);
+                int guess;
+                int guessCount = 0;
+                do
                 {
-                    for (int i = 0; i < numberArray.Count; i++)
+                    Console.WriteLine($"Guess {guessCount+1}");
+                    guess = GetRangedInput(1, maximum);
+                    guessCount++;
+                    if (guess < target)
                     {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}, {numberArray[i].Ticks} ticks.");
+                        Console.WriteLine("Higher!");
                     }
-                    Console.WriteLine("Press enter to return to menu.");
-                    Console.ReadLine();
-                }
-                else if (userSelection == 3)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
+                    else if (guess > target)
                     {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
+                        Console.WriteLine("Lower!");
                     }
-
-                    int modifyIndexParsed = GetValidInt("Please make a selection to modify: ");
-                    DateTime newItemParsed = DateTime.Now;
-                    numberArray[modifyIndexParsed - 1] = newItemParsed;
-                }
-                else if (userSelection == 4)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
-                    }
-                    int deleteIndexParsed = GetValidInt("Please make a selection to delete: ");
-                    numberArray.RemoveAt(deleteIndexParsed - 1);
-                }
-                else if (userSelection == 5)
-                {
-                    numberArray.Sort();
-                }
-            } while (userSelection != 0);
+                } while (guess != target);
+                Console.WriteLine($"You took {guessCount} guess(es), the answer was {target}.\n1. Play Again (Same Range)\n2. Play Again (New Range)\n3. Exit");
+                playAgain = GetRangedInput(1, 3);
+            } while (playAgain != 3);
         }
-        public static int GetValidInt(string prompt)
+
+
+        public static int GetRangedInput(int min, int max)
+        /*
+        Prompts the user for an integer between min and max inclusive until they make a valid input. 
+        If min is less than 1, or max is greater than 1000, throw an ArgumentOutOfRangeException.
+        */
         {
-            string input;
-            Console.Write(prompt);
-            input = Console.ReadLine().Trim().ToUpper();
-            int parsedInput;
-            while (!int.TryParse(input, out parsedInput))
+            int toReturn;
+            if (min < 1)
             {
-                Console.Write("Invalid input, please enter a integer (whole number) and try again: ");
-                input = Console.ReadLine().Trim().ToUpper();
+                throw new ArgumentOutOfRangeException(nameof(min));
             }
-            return parsedInput;
+            if (max > 1000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(max));
+            }
+            Console.Write($"Please enter a whole number between {min} and {max}, inclusive: ");
+            while (!int.TryParse(Console.ReadLine(), out toReturn) || !ValidateRangedInput(min, max, toReturn))
+            {
+                Console.Write($"Error, please try again. Please enter a whole number between {min} and {max}, inclusive.");
+            }
+            return toReturn;
+        }
+        public static bool ValidateRangedInput(int min, int max, int value)
+        /*
+        If value is greater than max, or less than min, return false.
+        Otherwise return true.
+        */
+        {
+            return min <= value && value <= max;
         }
     }
 }
--- CSharp1Demo/CSharpUnitTests/Tests.cs
+++ CSharp1Demo/CSharpUnitTests/Tests.cs
@@ -1,7 +1,33 @@
 ﻿namespace CSharpUnitTests
 {
-    public class DataStructures_Tests
+    public class Tests
     {
+        [Theory]
+        [InlineData(true, 1, 1000, 500)]
+        [InlineData(true, 1, 1000, 1)]
+        [InlineData(true, 1, 1000, 1000)]
+        [InlineData(false, 1, 1000, 0)]
+        [InlineData(false, 1, 1000, -1)]
+        [InlineData(false, 1, 1000, 1001)]
+        [InlineData(false, 1, 1000, int.MinValue)]
+        [InlineData(false, 1, 1000, int.MaxValue)]
+        [InlineData(true, int.MinValue, int.MaxValue, int.MinValue)]
+        [InlineData(true, int.MinValue, int.MaxValue, -1)]
+        [InlineData(true, int.MinValue, int.MaxValue, 0)]
+        [InlineData(true, int.MinValue, int.MaxValue, 1)]
+        [InlineData(true, int.MinValue, int.MaxValue, int.MaxValue)]
+        public static void Test_ValidateRangedInput(bool expectedResult, int min, int max, int value)
+        {
+            // Arrange
+            // N/A
+
+            // Act
+            bool actualResult = Program.ValidateRangedInput(min, max, value);   
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
 
     }
 }
