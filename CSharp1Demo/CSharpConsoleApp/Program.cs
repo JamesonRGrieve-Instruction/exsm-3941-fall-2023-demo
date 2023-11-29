accdc7e5f12fe6e048ceb0a9aac1884ad2c5c226
James Grieve <jgrieve@ualberta.ca>
2023-11-28 19:30:56 -0700

    Demo of 'fancy' lists;

--- CSharp1Demo/CSharpConsoleApp/Program.cs
+++ CSharp1Demo/CSharpConsoleApp/Program.cs
@@ -6,92 +6,54 @@ namespace CSharpConsoleApp
     {
         static void Main(string[] args)
         {
-            // Max length of 2.
-            string[] stringArray =
+            // All of these are types of 'collection', essentially they add functionality onto a base 'collection' type that stores values. Thus, they behave slightly differently.
+            List<int> intList = new List<int>();
+            // HashSets enforce the rule that all items must be unique.
+            HashSet<int> intHashSet = new HashSet<int>();
+            // SortedSet is a HashSet that also enforces that the items must be in order.
+            SortedSet<int> intSortedSet = new SortedSet<int>();
+            // Stack is a list with a "queue"-like function similar to a stack of papers on a desk. The most recent addition is the first to be processed (like the thing on top of the stack).
+            Stack<int> intStack = new Stack<int>();
+            // Queue is another "queue"-like function similar to, well, a queue. The first addition to the queue is the first one processed.
+            Queue<int> intQueue = new Queue<int>();
+
+            int[] thingsToAdd = new int[] { 1, 3, 42, 1, 5, 99, 42, 32, 15, 1, 12 };
+            foreach (int thing in thingsToAdd)
             {
-                "Hello",
-                "World"
-            };
-
-            // Max length of 10 (default values - null).
-            string[] emptyArray = new string[10];
-
-            // Null.
-            string emptyString;
-
-            // Max length of 10 (default values - 0).
-            int[] emptyIntArray = new int[10];
-
-            // 0.
-            int emptyInt;
-
-            // Using physical and logical sizes.
-            List<DateTime> numberArray = new List<DateTime>();
-
-            DateTime now = DateTime.Now;
-            TimeSpan difference = new TimeSpan(8, 0, 0, 0);
-            Console.WriteLine($"Exactly one week and one day from today will be {now.Add(difference)}.");
-
-            int userSelection;
-            do
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.BackgroundColor = ConsoleColor.White;
-                //Console.Clear();
-                userSelection = GetValidInt("--- Welcome to Number Storage ---\n1. Create - Add an Item\n2. Read - View the Items\n3. Update - Change an Item\n4. Delete - Remove an Item\n5. Sort - Organize the Items\n0. Exit\nPlease make a selection: ");
-                if (userSelection == 1)
-                {
-                    // Create
-                    DateTime dateTime = DateTime.Now;
-                    numberArray.Add(dateTime);
-                }
-                else if (userSelection == 2)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}, {numberArray[i].Ticks} ticks.");
-                    }
-                    Console.WriteLine("Press enter to return to menu.");
-                    Console.ReadLine();
-                }
-                else if (userSelection == 3)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
-                    }
-
-                    int modifyIndexParsed = GetValidInt("Please make a selection to modify: ");
-                    DateTime newItemParsed = DateTime.Now;
-                    numberArray[modifyIndexParsed - 1] = newItemParsed;
-                }
-                else if (userSelection == 4)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
-                    }
-                    int deleteIndexParsed = GetValidInt("Please make a selection to delete: ");
-                    numberArray.RemoveAt(deleteIndexParsed - 1);
-                }
-                else if (userSelection == 5)
-                {
-                    numberArray.Sort();
-                }
-            } while (userSelection != 0);
-        }
-        public static int GetValidInt(string prompt)
-        {
-            string input;
-            Console.Write(prompt);
-            input = Console.ReadLine().Trim().ToUpper();
-            int parsedInput;
-            while (!int.TryParse(input, out parsedInput))
-            {
-                Console.Write("Invalid input, please enter a integer (whole number) and try again: ");
-                input = Console.ReadLine().Trim().ToUpper();
+                intList.Add(thing);
+                intHashSet.Add(thing);
+                intSortedSet.Add(thing);
+                intStack.Push(thing);
+                intQueue.Enqueue(thing);
             }
-            return parsedInput;
+
+
+            // You lose some indexing options with some of these collection types, but you can always convert to an array if necessary.
+            Console.WriteLine(intSortedSet.ToArray()[2]);
+
+            // We have the ability to simultaneously remove an item from a Stack/Queue and use it for something.
+            Console.WriteLine($"Processing stack, item is: {intStack.Pop()}");
+            Console.WriteLine($"Processing queue, item is: {intQueue.Dequeue()}");
+
+            // Dictionaries can be indexed by anything, as opposed to everything above that are indexed by integers.
+            Dictionary<string, int> intDictionary = new Dictionary<string, int>();
+
+            intDictionary.Add("one", 1);
+            intDictionary.Add("two", 2);
+            intDictionary.Add("three", 3);
+            intDictionary.Add("four", 4);
+
+            Console.WriteLine(intDictionary["three"]);
+
+            // SortedLists are just like dictionaries mixed with SortedSets, whereby they enforce that the keys will always be in order.
+            SortedList<string, int> intSortedList = new SortedList<string, int>();
+
+            intSortedList.Add("one", 1);
+            intSortedList.Add("two", 2);
+            intSortedList.Add("three", 3);
+            intSortedList.Add("four", 4);
+
+
         }
     }
 }
