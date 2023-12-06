28caddcb56c4aa07b9ba37461aca91b6d6805e9c
James Grieve <jgrieve@ualberta.ca>
2023-12-05 19:44:53 -0700

    LINQ demo and data for complex example;

--- CSharp1Demo/CSharpConsoleApp/Program.cs
+++ CSharp1Demo/CSharpConsoleApp/Program.cs
@@ -6,92 +6,48 @@ namespace CSharpConsoleApp
     {
         static void Main(string[] args)
         {
-            // Max length of 2.
-            string[] stringArray =
+            List<int> myList = new List<int>() { 1, 5, 7, 12, 42, 3, 9, 32, 75, 99, 110, 47, 83, 63 };
+
+            // Where allows filtering of collections similar to .filter() in JavaScript.
+            List<int> myListFiltered = myList.Where((item) => item > 50).ToList();
+
+            List<int> filtered = new List<int>();
+            foreach (int item in myList)
             {
-                "Hello",
-                "World"
-            };
-
-            // Max length of 10 (default values - null).
-            string[] emptyArray = new string[10];
-
-            // Null.
-            string emptyString;
-
-            // Max length of 10 (default values - 0).
-            int[] emptyIntArray = new int[10];
-
-            // 0.
-            int emptyInt;
-
-            // Using physical and logical sizes.
-            List<DateTime> numberArray = new List<DateTime>();
-
-            DateTime now = DateTime.Now;
-            TimeSpan difference = new TimeSpan(8, 0, 0, 0);
-            Console.WriteLine($"Exactly one week and one day from today will be {now.Add(difference)}.");
-
-            int userSelection;
-            do
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.BackgroundColor = ConsoleColor.White;
-                //Console.Clear();
-                userSelection = GetValidInt("--- Welcome to Number Storage ---\n1. Create - Add an Item\n2. Read - View the Items\n3. Update - Change an Item\n4. Delete - Remove an Item\n5. Sort - Organize the Items\n0. Exit\nPlease make a selection: ");
-                if (userSelection == 1)
+                if (item > 50)
                 {
-                    // Create
-                    DateTime dateTime = DateTime.Now;
-                    numberArray.Add(dateTime);
+                    filtered.Add(item);
                 }
-                else if (userSelection == 2)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}, {numberArray[i].Ticks} ticks.");
-                    }
-                    Console.WriteLine("Press enter to return to menu.");
-                    Console.ReadLine();
-                }
-                else if (userSelection == 3)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
-                    }
-
-                    int modifyIndexParsed = GetValidInt("Please make a selection to modify: ");
-                    DateTime newItemParsed = DateTime.Now;
-                    numberArray[modifyIndexParsed - 1] = newItemParsed;
-                }
-                else if (userSelection == 4)
-                {
-                    for (int i = 0; i < numberArray.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}) {numberArray[i]}");
-                    }
-                    int deleteIndexParsed = GetValidInt("Please make a selection to delete: ");
-                    numberArray.RemoveAt(deleteIndexParsed - 1);
-                }
-                else if (userSelection == 5)
-                {
-                    numberArray.Sort();
-                }
-            } while (userSelection != 0);
-        }
-        public static int GetValidInt(string prompt)
-        {
-            string input;
-            Console.Write(prompt);
-            input = Console.ReadLine().Trim().ToUpper();
-            int parsedInput;
-            while (!int.TryParse(input, out parsedInput))
-            {
-                Console.Write("Invalid input, please enter a integer (whole number) and try again: ");
-                input = Console.ReadLine().Trim().ToUpper();
             }
-            return parsedInput;
+
+            // Select allows you to generate or select new values based on the current values similar to .map() in JavaScript.
+            List<int> modulusFives = myList.Select((item) => item%5).ToList();
+
+            // -- Aggregates --
+
+            // Count uses a Where-Like function.
+            int evenCount = myList.Where((item) => item % 2 == 0).Count();
+            int anotherEvenCount = myList.Count((item) => item % 2 == 0);
+
+            // Sum, average, min and max use a Select-Like function.
+            int evenSum = myList.Sum((item) => item % 5);
+            int evenMin = myList.Min((item) => item % 5);
+            int evenMax = myList.Max((item) => item % 5);
+            double evenAverage = myList.Average((item) => item % 5);
+            // Providing no argument runs the function on the items as-is.
+            double average = myList.Average();
+
+            // -- Ordering --
+            // Note that while we use a selection function in OrderBy, the result maintains the original items. This would not be the case if we did .Select().OrderBy().
+            List<int> orderedByModulusFives = myList.OrderBy(item => item % 5).ToList();
+            List<int> orderedByModulusFivesReversed = myList.OrderByDescending(item => item % 5).ToList();
+
+            List<int> orderedByModulusFivesThen = myList.OrderBy(item => item % 5).ThenBy(item=>item).ToList();
+
+            // -- Groups --
+            List<IGrouping<int, int>> grouped = myList.OrderBy(item => item % 5).GroupBy(item => item % 5).ToList();
+
         }
+
     }
 }
